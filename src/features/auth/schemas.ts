c2973d20500d7a5906471517f5657d7542515717
Bhushan Lagare <bhushanlagare7@gmail.com>
2026-01-27 20:20:03 +0530

    Add register auth api

--- src/features/auth/schemas.ts
+++ src/features/auth/schemas.ts
@@ -4,3 +4,9 @@ export const loginSchema = z.object({
   email: z.email(),
   password: z.string().min(1, "Password is required"),
 });
+
+export const registerSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.email(),
+  password: z.string().min(8, "Minimum of 8 characters are required"),
+});
